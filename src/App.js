c9018d810c35788991957a7a1eb266a957ac68a7
Marc Robert Wong <marcrobertwong@gmail.com>
2016-09-19 20:31:30 -0700

    Prototyping with /teams

--- src/App.js
+++ src/App.js
@@ -2,20 +2,28 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const ArrayList = ({ array }) => (
+    <ul>
+        {array.map(element =>
+                <li key={element}>
+                    {element}
+                </li>
+        )}
+    </ul>
+);
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h2>lcsbuilds-react</h2>
+                </div>
+                <ArrayList array={this.props.teams}/>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+// my imports
+import { createStore } from 'redux';
+import axios from 'axios';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+const teams = (state = [], action ) => {
+    switch (action.type) {
+        case 'TEAMS_FETCHED':
+            return action.teams
+        default:
+            return ['empty']
+    }
+}
+
+const store = createStore(teams);
+
+axios.get('/teams')
+    .then(function(response) {
+        store.dispatch({
+            type: 'TEAMS_FETCHED',
+            teams: response.data 
+        })
+    })
+    .catch(function(err) {
+        console.log(err);
+    });
+
+const render = () => {
+    ReactDOM.render(
+        <App teams={store.getState()}/>,
+        document.getElementById('root')
+    );
+};
+
+store.subscribe(render);
+render();
